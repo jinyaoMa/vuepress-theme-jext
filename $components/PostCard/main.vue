689874d6956453cb29b3fc66f72c75a5535770e2
jinyaoMa <jinyao.ma@outlook.com>
2020-12-29 23:29:19 -0500

    .

--- $components/PostCard/main.vue
+++ $components/PostCard/main.vue
@@ -1,19 +1,188 @@
 <template>
-  <div class="j-post-card"></div>
+  <div
+    class="j-post-card flex"
+    :class="{
+      'flex-horizontal': direction === 'horizontal',
+      'flex-vertical': direction === 'vertical',
+    }"
+    :style="{
+      borderRadius: round && '4px',
+      boxShadow: `0 2px 2px ${backColor}, 0 0 0 1px ${backColor}`,
+    }"
+  >
+    <router-link
+      class="j-post-card-cover background-image-fix"
+      :style="{
+        height,
+        width: maxWidth,
+        backgroundColor: backColor,
+        backgroundImage: `url('${options.cover}')`,
+      }"
+      :to="options.path"
+      :title="options.title"
+      tag="div"
+    ></router-link>
+    <div
+      class="flex-fill flex flex-vertical"
+      :style="{
+        padding: gap,
+      }"
+    >
+      <router-link
+        tag="div"
+        class="j-post-card-title"
+        :to="options.path"
+        :style="{
+          paddingBottom: gap,
+        }"
+      >
+        {{ options.title }}
+      </router-link>
+      <div
+        class="flex flex-horizontal flex-wrap"
+        :style="{
+          marginBottom: gap,
+        }"
+      >
+        <div class="j-post-card-date">
+          <i class="fas fa-calendar fa-fw"></i>
+          {{ options.date }}
+        </div>
+        <div class="j-post-card-updated">
+          <i class="fas fa-calendar-check fa-fw"></i>
+          {{ options.updated }}
+        </div>
+      </div>
+      <div
+        class="j-post-card-exceprt"
+        :style="{
+          height: `calc(100% - ${gap} * 3 - 3.4em)`,
+        }"
+        v-html="options.excerpt"
+      ></div>
+      <div
+        class="flex flex-horizontal flex-wrap flex-justify-between flex-align-center"
+        :style="{
+          marginTop: gap,
+        }"
+      >
+        <div class="j-post-card-categories">
+          <i class="fas fa-folder-open fa-fw"></i>
+          <span v-for="(item, i) in options.categories" :key="i">
+            <span v-if="i !== 0">, </span>
+            <router-link :to="item.path" :title="item.name">{{
+              item.name
+            }}</router-link>
+          </span>
+        </div>
+        <div>
+          <router-link class="j-post-card-more" :to="options.path" tag="div">{{
+            textMore
+          }}</router-link>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "j-post-card",
   props: {
+    direction: {
+      type: String,
+      default() {
+        return "horizontal";
+      },
+      validator(value) {
+        return ["vertical", "horizontal"].indexOf(value) !== -1;
+      },
+    },
+    height: {
+      type: String,
+      default() {
+        return "240px";
+      },
+    },
+    gap: {
+      type: String,
+      default() {
+        return "20px";
+      },
+    },
+    maxWidth: {
+      type: String,
+      default() {
+        return "320px";
+      },
+    },
+    backColor: {
+      type: String,
+      default() {
+        return "#e1e2e3";
+      },
+    },
+    round: {
+      type: Boolean,
+      default() {
+        return true;
+      },
+    },
+    textMore: {
+      type: String,
+      default() {
+        return "MORE";
+      },
+    },
     options: {
       type: Object,
       default() {
-        return {};
+        return {
+          date: "2020-02-22",
+          updated: "2020-12-22",
+          excerpt: "<p>Test</p>",
+          categories: [
+            {
+              name: "test",
+              path: "#",
+            },
+          ],
+          cover: "",
+          title: "Test",
+          path: "#",
+        };
       },
     },
   },
 };
 </script>
 
-<style lang="stylus"></style>
+<style lang="stylus">
+.j-post-card
+  position relative
+  overflow hidden
+  > div
+    box-sizing border-box
+  .j-post-card-cover, .j-post-card-more
+    cursor pointer
+    user-select none
+  .j-post-card-title
+    font-size 1.5em
+    line-height 1.2
+    font-weight 600
+    cursor pointer
+    width min-content
+  .j-post-card-date
+    margin-right 1em
+  .j-post-card-date, .j-post-card-updated, .j-post-card-categories, .j-post-card-more
+    width min-content
+    white-space nowrap
+    font-size 0.8em
+    line-height 1
+    opacity 0.6
+    transition opacity 0.2s
+    &:hover
+      opacity 1
+  .j-post-card-exceprt
+    overflow hidden
+</style>
--- $theme/components/RecentPosts.vue
+++ $theme/components/RecentPosts.vue
@@ -6,7 +6,11 @@
     hide-extend-icon
   >
     <div class="RecentPosts">
-      <j-post-card></j-post-card>
+      <j-post-card
+        v-for="(item, i) in posts"
+        :key="i"
+        :options="item"
+      ></j-post-card>
     </div>
   </j-container>
 </template>
@@ -14,7 +18,64 @@
 <script>
 export default {
   name: "RecentPosts",
+  computed: {
+    posts() {
+      const newest = this.j$SitePostsNewest.map((post) => {
+        const categories = [];
+        this.setCate(
+          categories,
+          post.frontmatter.categories,
+          "",
+          this.$category._metaMap
+        );
+        console.log(categories);
+
+        return {
+          date: post.frontmatter.date.substr(0, 10),
+          updated: post.frontmatter.updated.substr(0, 10),
+          excerpt: post.excerpt,
+          categories: categories,
+          cover: post.frontmatter.cover || "",
+          title: post.title,
+          path: post.path,
+        };
+      });
+    },
+    offset() {
+      return parseInt(this.$themeConfig.recentPostOffset || 3) + this.increment;
+    },
+  },
+  methods: {
+    setCate(cates, key, parent, map) {
+      if (typeof key === "string") {
+        cates.push({
+          name: key,
+          //path: map[parent === "" ? key : `${parent},${key}`].path,
+        });
+      } else if (key instanceof Array && key.length > 0) {
+        const _parent = parent === "" ? key[0] : `${parent},${key[0]}`;
+        const _key = key[1];
+        cates.push({
+          name: _parent,
+          //path: map[_parent].path,
+        });
+        this.setCate(cates, _key, _parent, map);
+      }
+    },
+    incrementOffset() {
+      this.increment += parseInt(this.$themeConfig.recentPostOffset) || 3;
+    },
+  },
+  data() {
+    return {
+      increment: 0,
+    };
+  },
 };
 </script>
 
-<style lang="stylus" scoped></style>
+<style lang="stylus" scoped>
+.RecentPosts
+  > .j-post-card
+    margin 40px
+</style>
